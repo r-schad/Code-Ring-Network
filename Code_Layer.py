aac8029c01952fd3d3ebc19f070c6b97369fa117
Robbie Schad <schadra@mail.uc.edu>
2023-12-13 18:45:03 -0500

    implement gaussian weights with strip code layer

--- Code_Layer.py
+++ Code_Layer.py
@@ -1,46 +1,68 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 class CodeLayer:
     '''
     A class implementing a code layer, organized in a ring-shape, that can take in an input, 
     and produces a sequence of activations to be passed into the ring layer.
     '''
-    def __init__(self, code_factor, ring_units) -> None:
+    def __init__(self, num_map_units, num_ring_units, code_factor, code_ring_spread=0.02) -> None:
         '''
-        :param code_factor int: number of neurons in code layer per ring neuron
-        :param ring_units int: number of neurons in ring layer
+        :param num_map_units int: number of neurons in the map layer.
+        :param num_ring_units int: number of neurons in the ring layer.
+        :param code_factor int: the  width of the code layer strip, i.e., the number of neurons in code layer per ring neuron.
+        :param code_ring_spread float: the standard deviation of the Gaussian curve used for determining the strength
+                            of weights from Code->Ring. 
+                            Default of 0.02 will give a weight of ~0.4 to the immediate neighbors of the corresponding
+                            ring unit, and a weight of ~0.02 to the neighbors 2-steps away from the peak ring unit for a
+                            given code unit.
         :returns: None
         '''
+        self.num_map_units = num_map_units
+        self.num_unique_units = num_ring_units
         self.code_factor = code_factor
-        self.ring_units = ring_units
-
-        self.code_units = self.code_factor * self.ring_units
+        self.num_units = num_ring_units * code_factor
+        self.num_ring_units = num_ring_units
         
+
+        self.weights_to_map_from_code = np.random.rand(num_map_units, self.num_units)
+        self.weights_to_ring_from_code = self.get_gauss_ring_weights(scale=code_ring_spread)
+        
+
+    def get_gauss_ring_weights(self, scale=0.02):
+        '''
+        Defines the weights from Code->Ring, which are either shaped in a circular strip,
+        with width=`self.code_factor`. These weights are based on a Gaussian curve, with 
+        each code unit's weights peaking at its corresponding ring unit. Depending on the value of 
+        `scale`, a given code unit may or may not correspond to a neighborhood of ring units, based
+        on the Gaussian curve used.
+
+        :param scale float: the standard deviation of the Gaussian curve used for determining the strength
+                            of weights from Code->Ring. 
+                            Default of 0.02 will give a weight of ~0.4 to the immediate neighbors of the corresponding
+                            ring unit, and a weight of ~0.02 to the neighbors 2-steps away from the peak ring unit for a
+                            given code unit.
+
+        :returns cov_mat_strip np.ndarray: the array of weights to ring from code layer, 
+            with shape (num_ring_units, num_code_units, code_factor).
+        '''
+        # define input into gaussian pdf
+        temp_gauss_arr = np.linspace(0, 1.0, self.num_ring_units)
+        # get gaussian pdf values - with standard deviation `scale`
+        weight_vals = norm.pdf(temp_gauss_arr, loc=temp_gauss_arr[len(temp_gauss_arr)//2], scale=scale)
+        # scale weight vals down so peak is 1.0
+        weight_vals_std = weight_vals / np.max(weight_vals)
+        # roll weight vals so peak is at index 0
+        weight_vals_rolled = np.roll(weight_vals_std, -1 * np.argmax(weight_vals_std))
+
         # create covariance matrix of code->ring weights
-        cov_mat = np.ndarray((self.code_units, self.ring_units))
-        
-        # get all weight values that will exist in the covariance matrix
-        weight_vals = np.linspace(0.0, 1.0, int(np.ceil(self.ring_units / 2))) # TODO consider changing linspace to use mexican-hat or steeper gaussian
+        cov_mat = np.ndarray((self.num_ring_units, self.num_unique_units))
 
-        # reflect weight values so the weight array is symmetric
-        weight_arr = np.hstack((weight_vals, np.flip(weight_vals[:-1])))
+        # store the weight values in each row of the covariance matrix, offset by 1 for each column
+        for i in range(self.num_ring_units):
+            cov_mat[:,i] = np.roll(weight_vals_rolled, i)
 
-        # store the weight values in each row of the covariance matrix, offset by 1 for each row
-        for i in range(self.ring_units):
-            cov_mat[i] = np.roll(weight_arr, i)
+        # duplicate the rows onto a new axis with dimension=code_factor
+        cov_mat_strip = np.repeat(cov_mat[:, :, np.newaxis], repeats=self.code_factor, axis=2)
 
-        # duplicate the rows so we have self.code_factor rows centered at each ring neuron
-        cov_mat = np.repeat(cov_mat[0:self.ring_units], repeats=self.code_factor, axis=0)
-        self.weights = np.random.rand(self.code_units, self.ring_units) * cov_mat     
-
-
-    def get_activations(self, input) -> np.ndarray:
-        '''
-        :param input np.ndarray: the input signal provided to the network
-        :returns: the sequence of activation values to be sent to the ring layer
-        '''
-
-        pass
-
-c = CodeLayer(3, 9)
+        return cov_mat_strip
