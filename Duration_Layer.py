11fd26a8420d4cf49c3ef36547228314c7b43bb7
Robbie Schad <schadra@mail.uc.edu>
2023-12-23 07:52:51 -0500

    Define duration layer, Still needs implemented.

--- Duration_Layer.py
+++ Duration_Layer.py
@@ -10,6 +10,7 @@ class DurationLayer:
         '''
         :param code_factor int: number of neurons in code layer per ring neuron
         :param ring_units int: number of neurons in ring layer
+
         :returns: None
         '''
         self.num_ring_units = num_ring_units
@@ -17,5 +18,12 @@ class DurationLayer:
         self.num_map_units = num_map_units
         self.weights_to_map_from_dur = np.ones((self.num_map_units, self.num_dur_units))
 
-    def activate(self, value) -> np.array:
+    def activate(self, value: float) -> np.array:
+        '''
+        returns an array `value`s, with length self.num_ring_units as a placeholder.
+
+        :param value (float, np.ndarray): the input value
+
+        :returns np.array: array of value, with length num_ring_units
+        '''
         return np.array([value] * self.num_ring_units)
