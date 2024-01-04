60e4529c710e50bf4a57a05edc43321b0bd399c7
Robbie Schad <schadra@mail.uc.edu>
2024-01-04 15:54:35 -0500

    Increase delta

--- Code_Ring_Network.py
+++ Code_Ring_Network.py
@@ -12,8 +12,38 @@ import os
 
 class CodeRingNetwork:
     def __init__(self, num_ring_units: int, num_code_units: int, code_factor: int, num_dur_units: int,
-                 map_d1: int, map_d2: int, init_map_lr: float = 0.1, init_map_nhood: float = 3, 
+                 map_d1: int, map_d2: int, init_map_lr: float = 0.1, init_map_nhood: float = 3,
                  activity_scale: float = 0.2, init_delta: float = 0.99) -> None:
+        '''
+        The class for the Code Ring Network. The architecture is as follows:
+            - Map Layer: A Self-Organizing Feature Map (SOFM) for encoding sequences
+                NOTE: The Map Layer has bidirectional weights <-> the code layer. These weights
+                are equivalent and remain equivalent throughout the learning process.
+            - Code Layer: A sequence-encoding layer for generating sequences for producing drawings
+            - Duration Layer: currently not implemented, right now just returns an appropriately-sized list
+                of whatever duration values are provided to the init() function
+            - Ring Layer: a spiking neural controller with dynamics for generating drawings 
+                based on inputs provided by the Code (and Duration) layers
+        
+        :param num_ring_units int: number of neurons in ring layer
+        :param num_code_units int: total number of neurons in code layer
+        :param code_factor int: width of the ring-shape in the code layer, i.e. how thick the strip is
+            NOTE: num_code_units / num_ring_units must equal code_factor
+        :param num_dur_units int: total number of neurons in duration layer. 
+            NOTE: Right now, num_dur_units should equal num_ring_units
+        :param map_d1 int: number of rows in the SOFM
+        :param map_d2 int: number of columns in the SOFM
+        :param init_map_lr float: initial SOFM learning rate (eta)
+            NOTE: this parameter is currently static throughout training
+        :param init_map_nhood float: inital SOFM neighborhood range (sigma_M)
+            NOTE: this parameter is currently static throughout training
+        :param activity_scale float: the scaling factor of the Map <-> Code weights, 
+            and the noise generated on the Code layer
+        :param init_delta float: the inital weighting value of the noise generated on the code layer 
+            as opposed to the weighting of the map signal propagated forward to the code layer
+
+        :returns: None
+        '''
         
         self.id_string = str(dt.now()).replace(':', '').replace('.','')
         print(f'ID string: {self.id_string}')
@@ -32,6 +62,7 @@ class CodeRingNetwork:
                                   init_map_lr, init_map_nhood, weight_scale=activity_scale)
         self.activity_scale = activity_scale
         # delta is the effect of code noise vs the map activity in determining code activity
+        self.init_delta = init_delta
         self.delta = init_delta
 
     def train(self, num_epochs: int, durations: float, t_max: int, t_steps: int, plot_gif: bool = False) -> list:
@@ -55,13 +86,14 @@ class CodeRingNetwork:
         scores = []
         for epoch in tqdm(range(num_epochs)):
             # get map activity by choosing a random winning neuron
-            map_winner_idx = np.random.choice(map_size)
+            rand_map_winner_idx = np.random.choice(map_size)
             # then activate the neighborhood around the winner
-            map_winner = self.map_layer.convert_to_coord(map_winner_idx)
-            map_signal = self.map_layer.neighborhood(map_winner)
+            rand_map_winner = self.map_layer.convert_to_coord(rand_map_winner_idx)
+            map_signal = self.map_layer.neighborhood(rand_map_winner)
+            
+            # propagate map signal forward
             weighted_map_signal = self.map_layer.weights_to_code_from_map @ map_signal.reshape(map_size, 1)
-            map_activation = sigmoid(v=weighted_map_signal, beta=1, mu=4)
-            gradient = map_activation * (1 - map_activation)
+            map_activation = weighted_map_signal / np.sum(map_signal) # TODO: figure out how to normalize this
 
             # apply random babbling signal into code layer
             code_noise = np.random.rand(self.code_layer.num_code_units, 1) * self.activity_scale
@@ -83,17 +115,27 @@ class CodeRingNetwork:
             # determine the most similar neuron to the activity of the code layer
             map_winner = self.map_layer.forward(code_input)
             # update the weights (bidirectionally, so both weight matrices M<->C) based on quality of the output
-            self.map_layer.update_weights(code_input, map_winner, gradient, doodle_score)
+            self.map_layer.update_weights(code_input, map_winner, doodle_score)
 
             # decrease the influence of the code babbling signal
-            self.delta = exponential(epoch, rate=-0.01, init_val=1.0)
+            self.delta = exponential(epoch, rate=-0.002, init_val=self.init_delta)
 
             print(f'Epoch {epoch}: {doodle_score}')
             scores += [doodle_score]
 
         return scores
     
-    def show_results(self, filename, durations, t_max, t_steps):
+    def show_results(self, filename, durations: float, t_max: int, t_steps: int) -> None:
+        '''
+        Saves the outputted drawing from each map neuron in one overall figure.
+
+        :param filename str: the filename of the outputted plot
+        :param durations float: the durations inputted into the code layer (this is temporary until duration layer is implemented)
+        :param t_max int: the maximum time to integrate each iteration over
+        :param t_steps int: the number of timesteps to integrate over from [0, t_max]
+
+        :returns: None
+        '''
         print('Saving Results Plot')
         map_size = self.map_layer.d1 * self.map_layer.d2
         fig, axs = plt.subplots(self.map_layer.d1, self.map_layer.d2, figsize=(self.map_layer.d1,self.map_layer.d2), sharex=True, sharey=True)
@@ -103,7 +145,7 @@ class CodeRingNetwork:
             (r, c) = self.map_layer.convert_to_coord(i)
             activity_matrix[r, c] = 1.0
             weighted_map_signal = self.map_layer.weights_to_code_from_map @ activity_matrix.reshape(map_size, 1)
-            map_activation = sigmoid(v=weighted_map_signal, beta=1, mu=4)
+            map_activation = weighted_map_signal / np.sum(activity_matrix)
 
             # determine output of code layer (input into ring layer)
             ring_input = self.code_layer.weights_to_ring_from_code @ map_activation
@@ -117,6 +159,7 @@ class CodeRingNetwork:
                                                               folder_name=self.folder_name, epoch=-1,
                                                               plot_gif=False, vars_to_plot={'v': False, 'u': False, 'z': False, 'I_prime': False})
             
+            # generate drawing for current neuron
             self.ring_layer.plot_final_doodle(ax=axs[r][c], xs=xs, ys=ys, intersec_pts=np.array([]), individualize_plot=False)
             axs[r][c].set_xlabel(f'{np.round(doodle_score,3)}')
             activity_matrix[r, c] = 0.0
@@ -128,19 +171,20 @@ class CodeRingNetwork:
 
     
 if __name__ == '__main__':
-    r = 24
-    c = 24
-    cf = 1
-    d = 24
+    r = 36
+    cf = 2
+    c = cf*r
+    d = 36
     durs = 0.2
     m_d1 = 8
     m_d2 = 8
     init_lr = 0.2
     init_map_sigma = 2
-    initial_delta = 1.0
-    num_epochs = 1000
+    initial_delta = 0.8
+    num_epochs = 500
     t_max = 30
     t_steps = 300
+    activity_scale = 1.0
     crn = CodeRingNetwork(num_ring_units=r,
                           num_code_units=c,
                           code_factor=cf,
@@ -148,15 +192,17 @@ if __name__ == '__main__':
                           map_d1=m_d1, map_d2=m_d2,
                           init_map_lr=init_lr,
                           init_map_nhood=init_map_sigma,
-                          activity_scale=1.0,
+                          activity_scale=activity_scale,
                           init_delta=initial_delta)
     
     id_string = crn.folder_name.split('\\')[-1]
 
-    crn.show_results(f'{crn.folder_name}\\initial_outputs_{id_string}.png', durs, t_max, t_steps)
-    scores = crn.train(num_epochs, durs, t_max, t_steps, plot_gif=False)
-    crn.show_results(f'{crn.folder_name}\\final_outputs_{id_string}.png', durs, t_max, t_steps)
+    sigmoid_mu = np.mean(crn.map_layer.neighborhood((0,0)) * crn.map_layer.d1 * crn.map_layer.d2) * activity_scale
+
+    crn.show_results(f'{crn.folder_name}\\initial_outputs_{id_string}.png', durs, t_max, t_steps, sigmoid_beta=1, sigmoid_mu=sigmoid_mu)
+    scores = crn.train(num_epochs, durs, t_max, t_steps, sigmoid_beta=1, sigmoid_mu=sigmoid_mu, plot_gif=False)
+    crn.show_results(f'{crn.folder_name}\\final_outputs_{id_string}.png', durs, t_max, t_steps, sigmoid_beta=1, sigmoid_mu=sigmoid_mu)
     plt.plot(range(num_epochs), scores)
-    plt.title(f'Scores over Time {id_string}')
+    plt.title(f'Scores Over Time {id_string}')
     plt.savefig(f'{crn.folder_name}\\all_scores_{id_string}.png')
     pass
